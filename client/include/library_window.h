da0432a81897b93f9a57e2b640a86f87abde6e0d
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-05 16:26:14 +0300

    Update library_window.h

--- client/include/library_window.h
+++ client/include/library_window.h
@@ -1,5 +1,5 @@
-#ifndef LIBRARY_H
-#define LIBRARY_H
+#ifndef LIBRARY_WINDOW_H
+#define LIBRARY_WINDOW_H
 
 #include <QGridLayout>
 #include <QGroupBox>
@@ -20,4 +20,4 @@ public:
     }
 };
 
-#endif  // LIBRARY_H
+#endif  // LIBRARY_WINDOW_H
