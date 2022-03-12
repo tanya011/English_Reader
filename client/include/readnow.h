d5a0e2caa9bbcafafbcfe199e3a043f4e53620b4
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-12 21:36:58 +0300

    Update readnow.h

--- client/include/readnow.h
+++ client/include/readnow.h
@@ -1,11 +1,53 @@
 #ifndef READNOW_H
 #define READNOW_H
 
-#include "sectionbase.h"
+#include <QApplication>
+#include <QClipboard>
+#include <QDebug>
+#include <QDesktopWidget>
+#include <QFile>
+#include <QGraphicsScene>
+#include <QGridLayout>
+#include <QLabel>
+#include <QListWidget>
+#include <QMenu>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QRect>
+#include <QString>
+#include <QTextEdit>
+#include <QTextStream>
+#include <QToolBar>
+#include <QWidget>
+#include <iostream>
+#include <QMainWindow>
 
-class ReadingNow:public SectionBase{
+namespace translate {
+std::string translate(const std::string &text);
+}
+
+class ReadNow : public QMainWindow {
 public:
-   ReadingNow(QWidget* parent = nullptr): SectionBase(parent){};
+    explicit ReadNow(QMainWindow *parent = nullptr);
+
+    ~ReadNow() override = default;
+
+private slots:
+    void translateText();
+
+private:
+    void printBook(const QString &book = nullptr);
+    void printWindowWithTranslate();
+    void buttonPhraseToDict();
+    void createActions();
+    void createToolBars();
+
+    int screen_width;
+    int screen_height;
+    QTextEdit *textEdit{};    // виджет, отображающий текст
+    QToolBar *fileToolBar{};  // панель управления
+    QTextEdit *text{};
+    QAction *translateSelectedText{};
 };
 
-#endif // READNOW_H
+#endif  // READNOW_H
