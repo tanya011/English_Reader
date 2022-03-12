032275d5bdbcb903c2a22adc83c76ecb97d468ce
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-12 23:44:23 +0300

    Add files via upload

---
+++ client/src/wordset.cpp
@@ -0,0 +1,21 @@
+#include <include/wordset.h>
+
+WordSet::WordSet(std::string title_): title(std::move(title_)){
+   id = ++count_for_id;
+}
+
+std::string WordSet::getName(){
+    return title;
+}
+
+void WordSet::addWord(Word &word){
+    words[word.getId()] = &word;
+}
+
+void WordSet::deleteWordById(int wordId){
+    words.erase(wordId);
+}
+
+[[nodiscard]]int WordSet::getId() const{
+    return id;
+}
