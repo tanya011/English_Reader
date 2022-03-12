fc43f9b95ebc57e6701f607c14c45bb6dd597c71
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-12 21:36:21 +0300

    Update sectionbase.h

--- client/include/sectionbase.h
+++ client/include/sectionbase.h
@@ -1,6 +1,10 @@
 #ifndef SECTIONBASE_H
 #define SECTIONBASE_H
 
+#include "library_window.h"
+#include "readnow.h"
+#include "dictionary.h"
+
 #include <QMainWindow>
 
 namespace Ui {
@@ -11,24 +15,29 @@ class SectionBase : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    LibraryWindow libraryWindow;
+    ReadNow readNow;
+    Dictionary dictionaryWindow;
+
 public:
     explicit SectionBase(QWidget *parent = nullptr);
     ~SectionBase();
 
-
     //кнопки на меню
-    QAction* collection;
+ //   QAction* collection;
     QAction* library;
     QAction* reading_now;
     QAction* dictionary;
+    /*
     QAction* cards;
     QAction* settings;
     QAction* entrance_exit;
-
+*/
 private:
     QMenu* sections;
 
-    Ui::SectionBase *ui;
+    Ui::SectionBase *ui; // TODO: better scoped_ptr
 };
 
 #endif // SECTIONBASE_H
