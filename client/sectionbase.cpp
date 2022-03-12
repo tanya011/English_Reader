b8692c41901a04cf6d5d1b97ec6d4386a165cb18
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-12 21:34:12 +0300

    Update sectionbase.cpp

--- client/sectionbase.cpp
+++ client/sectionbase.cpp
@@ -1,37 +1,53 @@
 #include "include/sectionbase.h"
-#include "ui_sectionbase.h"
 #include <QMenu>
 #include <QMenuBar>
+#include "ui_sectionbase.h"
 
-
-SectionBase::SectionBase(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::SectionBase)
-{
+SectionBase::SectionBase(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::SectionBase), readNow(), libraryWindow() {
     ui->setupUi(this);
 
-    //инициализация кнопок, если сделать класс для QActions - реализовать иниициализацию
-    collection = new QAction("Коллекция", parent);
-    library = new QAction("Каталог", parent);
-    reading_now = new QAction("Читаю сейчас", parent);
-    dictionary = new QAction("Словарь", parent);
-    cards = new QAction("Карточки", parent);
-    settings = new QAction("Настройки", parent);
-    entrance_exit = new QAction("Вход/Выход", parent);
+    //инициализация кнопок, если сделать класс для QActions - реализовать
+    //иниициализацию
+
+    library = new QAction("Библиотека", this);
+    reading_now = new QAction("Читаю сейчас", this);
+    //     collection = new QAction("Коллекция", parent);
+    dictionary = new QAction("Словарь", this);
+    //     cards = new QAction("Карточки", parent);
+    //     settings = new QAction("Настройки", parent);
+    //     entrance_exit = new QAction("Вход/Выход", parent);
 
     sections = menuBar()->addMenu("Меню");
 
-    sections->addAction(collection);
     sections->addAction(library);
     sections->addAction(reading_now);
+    //    sections->addAction(collection);
     sections->addAction(dictionary);
-    sections->addAction(cards);
-    sections->addAction(settings);
-    sections->addAction(entrance_exit);
+    //    sections->addAction(cards);
+    //    sections->addAction(settings);
+    //    sections->addAction(entrance_exit);
 
+    this->setWindowTitle("Читаю сейчас");
+    setCentralWidget(&readNow);
+
+    QObject::connect(reading_now, &QAction::triggered, this, [&]() {
+        this->setWindowTitle("Читаю сейчас");
+        takeCentralWidget();
+        setCentralWidget(&readNow);
+    });
+    QObject::connect(library, &QAction::triggered, this, [&]() {
+        this->setWindowTitle("Библиотека");
+        takeCentralWidget();
+        setCentralWidget(&libraryWindow);
+    });
+    QObject::connect(dictionary, &QAction::triggered, this, [&]() {
+        this->setWindowTitle("Словарь");
+        takeCentralWidget();
+        setCentralWidget(&dictionaryWindow);
+    });
 }
 
-SectionBase::~SectionBase()
-{
+SectionBase::~SectionBase() {
     delete ui;
 }
