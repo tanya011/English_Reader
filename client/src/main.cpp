1af58c41efb30a4d7dd9b0b07ee25c0489b85452
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-12 21:38:16 +0300

    Update main.cpp

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,41 +1,9 @@
 #include <QApplication>
-#include "include/collection.h"
-#include "include/library.h"
-#include "include/readnow.h"
-#include "include/dictionary.h"
-#include "include/cards.h"
-#include "include/settings.h"
-#include "include/entrance.h"
-#include "include/menuconnectuons.h"
+#include "include/sectionbase.h"
 
-void set_titles(Collection* cll, Library *lib, ReadingNow *rdn, Dictionary *dic, Cards *crd, Settings *stt, Entrance *ent){
-    cll->setWindowTitle("Коллекция");
-    lib->setWindowTitle("Каталог");
-    rdn->setWindowTitle("Читаю сейчас");
-    dic->setWindowTitle("Словарь");
-    crd->setWindowTitle("Карточки");
-    stt->setWindowTitle("Настройки");
-    ent->setWindowTitle("Вход/Выход");
-}
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-
-    //Создаём окна разделов
-    Collection* cll = new Collection;
-    Library *lib = new Library;
-    ReadingNow *rdn = new ReadingNow;
-    Dictionary *dic = new Dictionary;
-    Cards *crd = new Cards;
-    Settings *stt = new Settings;
-    Entrance *ent = new Entrance;
-
-    rdn->show();
-
-    set_titles(cll, lib, rdn, dic, crd, stt, ent);
-
-    all_connections(cll, lib, rdn, dic, crd, stt, ent);
-
+    SectionBase mainWindow;
+    mainWindow.show();
     return a.exec();
 }
