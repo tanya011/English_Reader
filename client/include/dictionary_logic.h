bcd1d4cd98c56a0a0d383044a4d1a7107817000e
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-17 16:04:23 +0300

    Update dictionary_logic.h

--- client/include/dictionary_logic.h
+++ client/include/dictionary_logic.h
@@ -15,12 +15,15 @@ public:
     WordSet all_words = WordSet("Все группы");
     std::map<int, WordSet> groups;
     int add_word(std::string original, std::string translation);
+    int add_word(std::string original, std::string translation, int id);
     void delete_word(int wordId);
     void add_word_to_group(int wordId, int groupId);
     void delete_word_from_group(int wordId, int wordsetId);
     int create_wordSet(std::string title);
+    int create_wordSet(std::string title, int id);
     void delete_wordSet(int wordset_Id);
     void add_allgroups_to_map();
+    ~DictionaryLogic();
 
 signals:
     void group_was_created(int wordset_id, std::string title, std::map<int, Word*> new_wordset);
