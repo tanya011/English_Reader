6ee95d65981aca46c8476c9afe48fe3c633cbe85
KonstantinosTrimiklinotis <90181273+KonstantinosTrimiklinotis@users.noreply.github.com>
2022-03-17 16:07:49 +0300

    Update dictionary_logic.cpp

--- client/src/dictionary_logic.cpp
+++ client/src/dictionary_logic.cpp
@@ -9,6 +9,15 @@ int DictionaryLogic::add_word(std::string original, std::string translation){
     return word.getId();
 }
 
+int DictionaryLogic::add_word(std::string original, std::string translation, int id){
+    Word word(std::move(original));
+    word.setTranslation(std::move(translation));
+    word.setId(id);
+    words[word.getId()] = word;
+    emit word_was_added_to_dictionary(word.getId());
+    return word.getId();
+}
+
 void DictionaryLogic::delete_word(int wordId){
 
     for (auto &group: groups){
@@ -40,6 +49,19 @@ int DictionaryLogic::create_wordSet(std::string title){
     return new_wordSet.getId();
 }
 
+int DictionaryLogic::create_wordSet(std::string title, int id){
+    for (auto groupName : groups){
+        if (groupName.second.getName() == title){
+            return groupName.second.getId();
+        }
+    }
+    WordSet new_wordSet(title);
+    new_wordSet.setId(id);
+    groups[new_wordSet.getId()] = new_wordSet;
+    emit group_was_created(new_wordSet.getId(), std::move(title), groups[new_wordSet.getId()].getWords());
+    return new_wordSet.getId();
+}
+
 void DictionaryLogic::delete_wordSet(int wordset_Id){
     emit wordset_was_deleted(wordset_Id);
     groups.erase(wordset_Id);
@@ -51,4 +73,27 @@ void DictionaryLogic::add_allgroups_to_map(){
 
 void DictionaryLogic::add_word_allgroups(int wordId){
     add_word_to_group(wordId, all_words.getId());
+}
+
+
+DictionaryLogic::~DictionaryLogic() {
+    std::ofstream file("dictionaryData.txt", std::ios::out); //файл лежит в cmake-build-debug
+    file << Word::counterSaver() << std::endl;
+    file << words.size() << std::endl;
+    for (auto& word: words){
+        file << word.second.getId() << std::endl;
+        file << word.second.getOriginal() << std::endl;
+        file << word.second.getTranslation() <<std::endl;
+    }
+    file << WordSet::counterSaver() << std::endl;
+    file << groups.size() << std::endl;
+    for (auto wordset: groups){
+        file << wordset.second.getWords().size() << std::endl;
+        file << wordset.second.getId() << std::endl;
+        file << wordset.second.getName() << std::endl;
+        for (auto word: wordset.second.getWords()){
+            file << word.second->getId() << std::endl;
+        }
+    }
+    file.close();
 }
