ee3012bffd8c944a01710a94b400213e362d6976
KonstantinosTrimikliniotis <kostenka177@yandex.ru>
2022-06-05 12:14:34 +0300

    thread bug fix

--- client/include/periodic_function.h
+++ client/include/periodic_function.h
@@ -19,33 +19,22 @@ public:
 
     void stop()
     {
-        _execute.store(false, std::memory_order_release);
-        if( _thd.joinable() )
-            _thd.join();
+        _execute = false;
     }
 
     void start(int interval, std::function<void(void)> func)
     {
-        if( _execute.load(std::memory_order_acquire) ) {
-            stop();
-        };
-        _execute.store(true, std::memory_order_release);
-        _thd = std::thread([this, interval, func]()
+        _execute = true;
+        std::thread([this, interval, func]()
                            {
-                               while (_execute.load(std::memory_order_acquire)) {
+                               while (_execute) {
                                    func();
                                    std::this_thread::sleep_for(
                                            std::chrono::milliseconds(interval));
                                }
-                           });
-    }
-
-    bool is_running() const noexcept {
-        return ( _execute.load(std::memory_order_acquire) &&
-                 _thd.joinable() );
+                           }).detach();;
     }
 
 private:
     std::atomic<bool> _execute;
-    std::thread _thd;
 };
